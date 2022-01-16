0df2085ebf5afde947b1f6178133809df12853cf
simon <simon.gaoy@gmail.com>
2022-01-16 21:36:26 +0800

    add bollinger strategy

---
+++ boll.py
@@ -0,0 +1,147 @@
+import backtrader as bt
+from datetime import datetime
+
+
+class BOLLStrat(bt.Strategy):
+    '''
+    This is a simple mean reversion bollinger band strategy.
+
+    Entry Critria:
+        - Long:
+            - Price closes below the lower band
+            - Stop Order entry when price crosses back above the lower band
+        - Short:
+            - Price closes above the upper band
+            - Stop order entry when price crosses back below the upper band
+    Exit Critria
+        - Long/Short: Price touching the median line
+    '''
+
+    params = (("period", 20), ("devfactor", 2), ("size", 20), ("debug", False))
+
+    def __init__(self):
+        self.boll = bt.indicators.BollingerBands(period=self.p.period,
+                                                 devfactor=self.p.devfactor)
+        #self.sx = bt.indicators.CrossDown(self.data.close, self.boll.lines.top)
+        #self.lx = bt.indicators.CrossUp(self.data.close, self.boll.lines.bot)
+
+    def next(self):
+
+        orders = self.broker.get_orders_open()
+
+        # Cancel open orders so we can track the median line
+        if orders:
+            for order in orders:
+                self.broker.cancel(order)
+
+        if not self.position:
+
+            if self.data.close > self.boll.lines.top:
+
+                self.sell(exectype=bt.Order.Stop,
+                          price=self.boll.lines.top[0],
+                          size=self.p.size)
+
+            if self.data.close < self.boll.lines.bot:
+                self.buy(exectype=bt.Order.Stop,
+                         price=self.boll.lines.bot[0],
+                         size=self.p.size)
+
+        else:
+
+            if self.position.size > 0:
+                self.sell(exectype=bt.Order.Limit,
+                          price=self.boll.lines.mid[0],
+                          size=self.p.size)
+
+            else:
+                self.buy(exectype=bt.Order.Limit,
+                         price=self.boll.lines.mid[0],
+                         size=self.p.size)
+
+        if self.p.debug:
+            print(
+                '---------------------------- NEXT ----------------------------------'
+            )
+            print("1: Data Name:                            {}".format(
+                data._name))
+            print("2: Bar Num:                              {}".format(
+                len(data)))
+            print("3: Current date:                         {}".format(
+                data.datetime.datetime()))
+            print('4: Open:                                 {}'.format(
+                data.open[0]))
+            print('5: High:                                 {}'.format(
+                data.high[0]))
+            print('6: Low:                                  {}'.format(
+                data.low[0]))
+            print('7: Close:                                {}'.format(
+                data.close[0]))
+            print('8: Volume:                               {}'.format(
+                data.volume[0]))
+            print('9: Position Size:                       {}'.format(
+                self.position.size))
+            print(
+                '--------------------------------------------------------------------'
+            )
+
+    def notify_trade(self, trade):
+        if trade.isclosed:
+            dt = self.data.datetime.date()
+
+            print(
+                '---------------------------- TRADE ---------------------------------'
+            )
+            print("1: Data Name:                            {}".format(
+                trade.data._name))
+            print("2: Bar Num:                              {}".format(
+                len(trade.data)))
+            print("3: Current date:                         {}".format(dt))
+            print('4: Status:                               Trade Complete')
+            print('5: Ref:                                  {}'.format(
+                trade.ref))
+            print('6: PnL:                                  {}'.format(
+                round(trade.pnl, 2)))
+            print(
+                '--------------------------------------------------------------------'
+            )
+
+
+#Variable for our starting cash
+startcash = 10000
+
+# Create an instance of cerebro
+cerebro = bt.Cerebro()
+
+# Add our strategy
+cerebro.addstrategy(BOLLStrat)
+
+data = bt.feeds.GenericCSVData(dataname='601318.csv',
+                                dtformat=('%Y-%m-%d'),
+                                datetime=0,
+                                open=1,
+                                high=2,
+                                low=3,
+                                close=4,
+                                volumn=5,
+                                openinterest=6)
+
+# Add the data to Cerebro
+cerebro.adddata(data)
+
+# Add a sizer
+cerebro.addsizer(bt.sizers.FixedReverser, stake=10)
+
+# Run over everything
+cerebro.run()
+
+#Get final portfolio Value
+portvalue = cerebro.broker.getvalue()
+pnl = portvalue - startcash
+
+#Print out the final result
+print('Final Portfolio Value: ${}'.format(round(portvalue, 2)))
+print('P/L: ${}'.format(round(pnl, 2)))
+
+# Finally plot the end results
+cerebro.plot(style='candlestick')
